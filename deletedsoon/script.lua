29fed044d2237a88f1c0ac87750a955912431528
DexxterGWM <99261879+DexxterGWM@users.noreply.github.com>
2024-03-18 18:56:19 -0300

    script.lua

--- deletedsoon/script.lua
+++ deletedsoon/script.lua
@@ -127,7 +127,9 @@ local function getNpcPrompt() : ()
 		
 		fireproximityprompt(npcPrompt, 1, true)
 
-		SimpleSpy:GetRemoteFiredSignal(game:GetService('ReplicatedStorage').Packages._Index['sleitnick_knit@1.4.7'].knit.Services.HackingService.RE.StartedPhoneHack):Connect(function()
+		SimpleSpy:GetRemoteFiredSignal(game:GetService('ReplicatedStorage').Packages._Index['sleitnick_knit@1.4.7'].knit.Services.HackingService.RE.StartedPhoneHack):Connect(function(a, b)
+			print('a:', a, '| b:', b) --
+			
 			hackedGuiFrame.Visible = false
 			finishedEvent:FireServer(tostring(npc.Name))
 		end)
