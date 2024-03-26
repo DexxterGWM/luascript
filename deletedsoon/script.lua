c6b23d7a18b12e10fc65af7cf817cf46ba59b989
DexxterGWM <99261879+DexxterGWM@users.noreply.github.com>
2024-03-26 18:01:04 -0300

    script.lua

--- deletedsoon/script.lua
+++ deletedsoon/script.lua
@@ -1,262 +1,157 @@
-if not (game:IsLoaded()) then game.Loaded:Wait() end
-
--- [[ SERVICES VARIABLES ]]
+-- [[ ESSENTIAL ]]
 
 -- API(s)
-loadstring(game:HttpGet('https://github.com/exxtremestuffs/SimpleSpySource/raw/master/SimpleSpy.lua'))()
+pcall(function() loadstring(game:HttpGet('https://github.com/exxtremestuffs/SimpleSpySource/raw/master/SimpleSpy.lua'))() end)
 
--- GAME SERVICES
-local ReplicatedStorage = game:GetService('ReplicatedStorage')
-local Workspace = game:GetService('Workspace')
+-- [[ MODULES ]]
 
--- [[ MODULES/REMOTES FOLDERS ]]
+-- MODULES FOLDERS
+local Controllers : Folder = game:GetService('ReplicatedStorage').Source.Controllers
 
--- MODULES
-local Source = ReplicatedStorage.Source
-local Controllers = Source.Controllers
+-- MODULES VARIABLES
+local HackingController : {} = require(Controllers.HackingController)
 
--- REMOTES
-local Packages : Folder = ReplicatedStorage.Packages
-local Index : Folder = Packages._Index['sleitnick_knit@1.4.7']
-local knit : Folder = Index.knit
-local Services : Folder = knit.Services
-local HackingService : Folder = Services.HackingService
-local RE = HackingService.RE
+-- [[ REMOTES ]]
 
--- [[ MODULES/REMOTES VARIABLES ]]
+-- REMOTES FOLDERS
+local RE : Folder = game:GetService('ReplicatedStorage').Packages._Index['sleitnick_knit@1.4.7'].knit.Services.HackingService.RE
 
--- MODULES
-local HackingController = require(Controllers.HackingController)
-
--- REMOTES
-local StartedPhoneHack = RE.StartedPhoneHack
-local FinishedPhoneHack = RE.FinishedPhoneHack
+-- REMOTES VARIABLES
+local StartedPhoneHack : RemoteEvent = RE.StartedPhoneHack
+local FinishedPhoneHack : RemoteEvent = RE.FinishedPhoneHack
 
 -- [[ SCRIPT VARIABLES ]]
 
--- PLAYER VARIABLES
-local player = game:GetService('Players').LocalPlayer
-local playerChar = player.Character
+-- TABLES
+local npcsTabl : {[string] : Instance} = {}
 
--- may won't need these*
--- local hackedGui = player.PlayerGui.PhoneHackDialog
--- local hackedGuiFrame = hackedGui.Holder
-local NPCHackDialog = player.PlayerGui.NPCHackDialog
+-- FUNCTIONS
+fireproximityprompt = fireproximityprompt
+local pairs = pairs
 
--- WORKSPACE
-local npcFolder : Fodler = Workspace.NPC
+-- [[ LOCAL FUNCTIONS ]]
 
--- FUNCTIONS VARIABLES
-fireproximity = fireproximity
-
-local taskDefer = task.defer
-local xpCall = xpcall
-
--- [[ TABLES ]]
-local npcsStatesTabl = {
-	[1] = 'loaded'
-	-- [2] = 'preunloaded',
-	-- [3] = 'unloaded'
-}
-
-local npcsTabl = {}
-local npcsPlacesTabl = {}
-
--- [[ FUNCTIONS ]]
-
--- AUX FUNCTIONS
-local function call(funct, args, ...) : ()
-	local success, returnVal = xpCall(
-		function()
-			if not (args) then funct()
-			elseif args then funct(args)
-			end
-		end,
-
-		function(err) print(('%s...'):format(err)) end
-	)
-
-	return (success and true) or (returnVal and false)
-end
-
--- FUNCTIONAL FUNCTIONS
-local function delNpcs(npc) : ()
-	local npc = rawget(npc, 1)
-	if not (npc) then return end
-
-	if rawget(npcsTabl, npc.Name) then
-		table.remove(npcsTabl, table.find(npcsTabl, npc.Name))
-		print(('[*] %s Got deleted from <npcs>'):format(tostring(npc.Name)))
-	end
+-- SETTERS
+local function setNpc(npcName : string, npc : Instance) : ()
+	npcsTabl[npcName] = npc
+	print(('[*] %s setted on <npcs>'):format(tostring(npcName)))
 
 	return
 end
 
-local function setNpcs(npc) : ()
-	local npc = rawget(npc, 1)
-	if not (npc) then return end
-
-	if rawget(npcsTabl, npc.Name) then return end
-
-	rawset(npcsTabl, npc.Name, {
-		['npc'] = npc,
-		['state'] = rawget(npcsStatesTabl, (rawget(npcsPlacesTabl, npc.Parent.Name)))
-	})
-
-	print(('[*] %s Got added on <npcs>'):format(tostring(npc.Name)))
+local function delNpc(npcName : string) : ()
+	table.remove(npcsTabl, table.find(npcsTabl, npcName))
+	print(('[*] %s deleted from <npcs>'):format(tostring(npcName)))
 
 	return
 end
 
-local function childEvents(child) : ()
-	local event = rawget(child, 2)
-	local child = rawget(child, 1)
+-- FUNCTIONAL
+local function npcIterator(childTabl : {[number] : Instance}) : ()
+	local npcIteratorThread = coroutine.create(function(_) : ()
+		for index = 1, #childTabl do
+			if (childTabl[index]:IsA('Model') and childTabl[index]:FindFirstChildOfClass('Humanoid')) then
+				if (rawget(npcsTabl, childTabl[index].Name)) then
+					coroutine.yield({['success'] = false})
 
-	if not (child or event ~= nil) then return
-	elseif not (typeof(child) == 'Instance') then return
-	end
+					continue
+				end
 
-	if child.ClassName == 'Model' then
-		if not (rawget(npcsTabl, child.Name)) then
-			if event then call(setNpcs, {child})
-			elseif not (event) then call(delNpcs, {child})
+				coroutine.yield({['success'] = true, ['npc'] = childTabl[index]})
 			end
+		end
+	end)
+
+	return function() : {}
+		local _, tabl : {['string'] : boolean | Instance} = coroutine.resume(npcIteratorThread) -- coroutine.close
+
+		return tabl
+	end
+end
+
+local function npcHandler(childTabl : {[number] : Instance}) : ()
+	for npcTabl in npcIterator(childTabl) do
+		if (npcTabl['success']) then
+			setNpc(rawget(npcTabl, 'npc'))
+
+		elseif (not (npcTabl['success'])) then
+			warn(('%s already setted, continuing'):format(tostring(rawget(npcTabl, 'npc')))) --
+
+			continue
 		end
 	end
 
 	return
 end
 
-local function getNpcPrompt() : ()
-	for npc, npcTabl in pairs(npcsTabl) do
-		local npc = npcTabl['npc']
-		
-		local npcRootPart = npc.HumanoidRootPart
-		playerChar:MoveTo(npcRootPart.Position)
-		
-		local npcPrompt = npcRootPart:FindFirstChild('ProximityPrompt')
+local function getPrompt(npc : Instance) : boolean -- prompt type
+	if (not (npc.ClassName == 'Model' and npc:FindFirstChildOfClass('Humanoid'))) then warn('error getting prompt'); return false end
+	print(('attempt to get prompt of %s'):format(tostring(npc.Name))) --
+	
+	local prompt -- :? type
 
-		while not (npcPrompt) do -- GetAttributeChangedSignal?
-			wait(1)
-			
-			playerChar:MoveTo(npcRootPart.Position)
-			npcPrompt = npcRootPart:FindFirstChild('ProximityPrompt')
-		end
+	local connection; connection = npc:GetAttributeChangedSignal('NextCFrame'):Connect(function()
+		game:GetService('Players').LocalPlayer.Character:MoveTo(npc.HumanoidRootPart.Position)
+		prompt = npc.HumanoidRootPart:FindFirstChild('ProximityPrompt')
 
-		-- [[ need abstraction ("fireNpcPrompt" may)
-		
-		fireproximityprompt(npcPrompt, 1, true)
-		
-		local waitFor = false
+		if (prompt) then connection:Disconnect() end
+	end)
 
-		SimpleSpy:GetRemoteFiredSignal(StartedPhoneHack):Connect(function(npc)
-			print('Finished')
-			FinishedPhoneHack:FireServer(0)
-		end)
-		
-		SimpleSpy:GetRemoteFiredSignal(FinishedPhoneHack):Connect(function()
-			local guiConnection; guiConnection = NPCHackDialog:GetPropertyChangedSignal('Enabled'):Connect(function()
-				guiConnection:Disconnect()
-				waitFor = true
-			end)
-			
-			HackingController.CancelAndCleanFromOutside()
-		end)
-		
-		while not (waitFor) do wait(1) end
-
-		call(delNpcs, {npc})
+	while (not (prompt)) do
+		if (not (npc.Parent)) then warn(('failed getting prompt of %s'):format(tostring(npc.Name))); return false end
 		wait(1)
-		
-		-- ]]
-	end
-end
-
-local function setNpcsFromPath(path) : ()
-	local path = rawget(path, 1)
-	if not (path) then return end
-
-	for _, npc in pairs(path:GetDescendants()) do
-		if not (npc.ClassName == 'Model') then continue end
-		if rawget(npcsTabl, npc.Name) then continue end
-
-		local success, returnVal = call(setNpcs, {npc})
-
-		if success then
-			-- ?
-			
-			-- npc.AncestryChanged:Connect(function(npc, parent)
-			--	if not (npc.Parent) then call(delNpcs, {npc})
-			--	elseif npc.Parent then print(npc.Name, 'New parent:', parent)
-			--	end
-			-- end)
-
-		elseif not (success) then print('[-] Failed on <npcs path>:', returnVal)
-		end
 	end
 	
-	call(getNpcPrompt)
+	print(('got %s of %s | %s'):format(tostring(prompt.Name), tostring(npc.Name), typeof(prompt))) --
 
-	-- path.ChildAdded:Connect(function(child)
-	--	call(childEvents, {[1] = child, [2] = true})
-	-- end)
-	
-	-- path.ChildRemoved:Connect(function(child)
-	--	call(childEvents, {[1] = child, [2] = false})
-	-- end)
-
-	return
+	return prompt
 end
 
-local function setNpcsPlaces(path) : ()
-	for ind = 1, #npcsStatesTabl do
-		if rawget(path, ind) then
-			rawset(npcsPlacesTabl, rawget(path, ind).Name, ind)
+local function firePrompt(prompt) : boolean
+	local waitFor = false
+
+	local connection; connection = game:GetService('Players').LocalPlayer.PlayerGui.PhoneHackDialog.Holder:GetPropertyChangedSignal('Visible'):Connect(function()
+		connection:Disconnect()
+		HackingController.CancelAndCleanFromOutside()
+	end)
+
+	fireproximityprompt(prompt, 1, true)
+
+	--SimpleSpy:GetRemoteFiredSignal(FinishedPhoneHack):Connect(function()
+	--	waitFor = true
+	--end)
+
+	--SimpleSpy:GetRemoteFiredSignal(StartedPhoneHack):Connect(function(npc)
+	--	FinishedPhoneHack:FireServer(0)
+	--end)
+
+	while not (waitFor) do wait(1) end
+
+	return true
+end
+
+local function npcPromptHandler() : ()
+	for _, npc in pairs(npcsTabl) do
+		local prompt = getPrompt(npc)
+
+		if (prompt) then
+			firePrompt(prompt)
+			delNpc(npc.Name)
 		end
 	end
 
 	return
 end
 
--- OTHERS FUNCTIONS
-local function getNpcsPlaces() : ()
-	local success, returnVal = call(setNpcsPlaces, {
-		[1] = npcFolder
-		-- [2] = preUnloadedNPC,
-		-- [3] = unloadedNPC
-	})
-
-	if success then print('[+] Success on <npcs places>')
-	elseif not (success) then print('[-] Failed on <npcs places>:', returnVal)
-	end
-
-	return
-end
-
-local function getLoadedNpc() : ()
-	local success, returnVal = call(setNpcsFromPath, {npcFolder})
-
-	if success then print('[+] Success on <loaded npcs>')
-	elseif not (success) then print('[-] Failed on <loaded npcs>:', returnVal)
-	end
-
-	return
-end
-
--- getPreUnloadedNpc()
+npcHandler(game:GetService('Workspace').NPC:GetChildren())
+-- game:GetService('Lighting').UnloadedNPC
 -- game:GetService('Lighting').PreUnloadedNPC
 
--- getUnloadedNpc() : ()
--- game:GetService('Lighting').UnloadedNPC
+npcPromptHandler()
 
--- [[ SCRIPT ]]
-
--- SETTINGS
-call(getNpcsPlaces)
-
--- STARTING
-call(getLoadedNpc)
--- preUnloadedNpc
--- unloadedNpc
-
--- taskDefer
+-- local test; test = npcFolder.ChildAdded:Connect(function(child : Instance) : ()
+-- 	if (not (child:FindFirstChildOfClass('Humanoid'))) then return end
+-- 	
+-- 	npcHandler({child})
+-- 	
+-- 	return
